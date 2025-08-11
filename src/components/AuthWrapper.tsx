909253700915aeca22a32b4d9d9684d86948c71e
Darcy Liu <d273liu@uwaterloo.ca>
2025-08-11 13:52:18 -0400

    Remove all Clerk.js dependencies and code

--- src/components/AuthWrapper.tsx
+++
@@ -1,9 +0,0 @@
-interface AuthWrapperProps {
-  children: React.ReactNode;
-}
-
-function AuthWrapper({ children }: AuthWrapperProps): JSX.Element {
-  return children;
-}
-
-export default AuthWrapper;
