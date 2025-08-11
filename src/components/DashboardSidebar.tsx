60eba1705ae8b2843aa299b6aaad52815215f8a7
Darcy Liu <d273liu@uwaterloo.ca>
2025-08-11 13:49:40 -0400

    Updated DashboardSidebar.tsx

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -45,15 +45,7 @@ function DashboardSidebar(): JSX.Element {
 
   return (
     <div className="w-64 bg-gray-900 border-r border-gray-800 min-h-screen flex flex-col">
-      <div className="p-6 border-b border-gray-800">
-        <Link to="/dashboard" className="flex items-center">
-          <img
-            src="/images/logo_ngb.png"
-            alt="DKK Logo"
-            className="w-8 h-8 mr-3"
-          />
-        </Link>
-      </div>
+      
 
       <nav className="flex-1 p-4">
         <ul className="space-y-2">
