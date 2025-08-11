255209c215b6aeeffe68f050c50400264802ac52
Darcy Liu <d273liu@uwaterloo.ca>
2025-08-11 15:05:19 -0400

    Added tsconfig.node.tsbuildinfo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+var config = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FFA500",
+                secondary: "#4A4A4A",
+            },
+        },
+    },
+    plugins: [],
+};
+export default config;
