46c27b538f83fa5e03d1b0705d2d7d3c54d07cc9
Darcy Liu <d273liu@uwaterloo.ca>
2025-08-11 09:22:27 -0400

    Added tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFA500",
+        secondary: "#4A4A4A",
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
