77b9edc0598b87314ef182c3cb1b2341a7ab6fa9
Darcy Liu <d273liu@uwaterloo.ca>
2025-08-11 10:54:51 -0400

    Add navbar alongside vertical sidebar in dashboard

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,16 +1,21 @@
 import DashboardSidebar from './DashboardSidebar';
 
+import Header from './Header';
+
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 function DashboardLayout({ children }: DashboardLayoutProps): JSX.Element {
   return (
-    <div className="flex min-h-screen bg-black">
-      <DashboardSidebar />
-      <main className="flex-1 overflow-auto">
-        {children}
-      </main>
+    <div className="min-h-screen bg-black">
+      <Header />
+      <div className="flex">
+        <DashboardSidebar />
+        <main className="flex-1 overflow-auto">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
